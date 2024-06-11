cd5b6d4bffb78d747e2634c75eaf69e4c60b5a15
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2024-06-11 11:09:24 -0500

    Add article edit and DELETE endpoint

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  skip_forgery_protection
+
   def index
     @articles = Article.all
   end
@@ -19,8 +21,29 @@ class ArticlesController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
-  end  
-  
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
   private
     def article_params
       params.require(:article).permit(:title, :body)
